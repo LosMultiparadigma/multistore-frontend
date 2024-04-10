ac179438b1038f84b1a515c652fd74873873c0ee
arielmerinos <arielmerino@ciencias.unam.mx>
2024-04-09 20:01:09 -0600

    Update favicon link and page title in index.html

--- multistore/src/App.tsx
+++ multistore/src/App.tsx
@@ -1,16 +1,12 @@
-
-import './App.css'
+import "./App.css";
 
 export default function App() {
   return (
     <>
-        <h1 className="text-3xl font-bold underline">
-      Hello World! fron Viiiiite
-    </h1>
-    <h2 className="text-2xl font-bold">
-      Welcome to MultiStore
-    </h2>
+      <h1 className="text-3xl font-bold underline">
+        Hello World! fron Viiiiite
+      </h1>
+      <h2 className="text-2xl font-bold">Welcome to MultiStore</h2>
     </>
-  )
+  );
 }
-
