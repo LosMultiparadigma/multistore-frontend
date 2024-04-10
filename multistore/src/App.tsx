b436c6603319703bf967b27b4b99ec63d44c4c86
arielmerinos <arielmerino@ciencias.unam.mx>
2024-04-09 21:51:47 -0600

    Update welcome message in App.tsx

--- multistore/src/App.tsx
+++ multistore/src/App.tsx
@@ -6,7 +6,8 @@ export default function App() {
       <h1 className="text-3xl font-bold underline">
         Ay oli 🤣
       </h1>
-      <h2 className="text-2xl font-bold">Welcome to MultiStore (;</h2>
+      <h2 className="text-2xl font-bold">Welcome to MultiStore 🏬</h2>
+      <h2 className="text-2xl font-bold">Hoy no estamos, regrese otro día 🌞</h2>
     </>
   );
 }
